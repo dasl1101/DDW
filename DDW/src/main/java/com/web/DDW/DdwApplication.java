54944637d65110bde7d7ae7547ecc3829d5ae406
dasl1101 <ektmf1101@gmail.com>
2023-06-20 14:25:06 +0900

    initial commit

---
+++ DDW/src/main/java/com/web/DDW/DdwApplication.java
@@ -0,0 +1,13 @@
+package com.web.DDW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdwApplication.class, args);
+	}
+
+}
