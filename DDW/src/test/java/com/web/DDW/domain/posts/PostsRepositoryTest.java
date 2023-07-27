19869196e4bcd935d8c428d48af3281fa31dadc7
dasl1101 <ektmf1101@gmail.com>
2023-07-27 14:31:37 +0900

    [chore] test method name

--- DDW/src/test/java/com/web/DDW/domain/posts/PostsRepositoryTest.java
+++ DDW/src/test/java/com/web/DDW/domain/posts/PostsRepositoryTest.java
@@ -24,7 +24,7 @@ class PostsRepositoryTest {
     }
 
     @Test
-    public void 게시글저장_불러오기(){
+    public void postSave(){
         //given
         String title = "테스트 게시글";
         String content = "테스트 본문";
